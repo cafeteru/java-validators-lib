5a224527063eb86a7c318e8dee77086aa7a18974
Iván González <cafeteru.dev@gmail.com>
2022-12-25 15:59:51 +0100

    Create ValidatorDni

--- src/test/java/com/cafeteru/validatorLib/AppTest.java
+++ src/test/java/com/cafeteru/validatorLib/AppTest.java
@@ -1,18 +1,18 @@
 package com.cafeteru.validatorLib;
 
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest {
+class AppTest {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue() {
+    void shouldAnswerWithTrue() {
         assertTrue(true);
     }
 }
