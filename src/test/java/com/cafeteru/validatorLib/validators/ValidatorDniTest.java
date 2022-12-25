d803583160ec78573e7285a2b65e25127389e7a4
Iván González <cafeteru.dev@gmail.com>
2022-12-25 20:27:11 +0100

    Config maven wrapper

--- src/test/java/com/cafeteru/validatorLib/validators/ValidatorDniTest.java
+++ src/test/java/com/cafeteru/validatorLib/validators/ValidatorDniTest.java
@@ -7,19 +7,24 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import com.cafeteru.validatorLib.validators.ValidatorDni;
-
 
 /**
  * Check class ValidatorDni
  */
 class ValidatorDniTest {
 
-//    @Test
-//    void valid_data() {
-//        var validator = new ValidatorDni(RandomMethods.dni());
-//        Assertions.assertTrue(validator.isValid());
-//    }
+    @ParameterizedTest
+    @ValueSource(strings = {
+        "14892253Y",
+        "37468912A",
+        "19484773R",
+        "66690812N",
+        "14718986K"
+    })
+    void valid_data(String dni) {
+        var validator = new ValidatorDni(dni);
+        assertTrue(validator.isValid());
+    }
 
     @Test
     void null_data() {
